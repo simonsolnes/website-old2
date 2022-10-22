40e6d83a5daad3857a848e961760461a2f6e5bb5
Simon Solnes <simon@solnes.co>
2022-10-22 13:42:07 +0200

    Query parameters parser

--- src/url.rs
+++ src/url.rs
@@ -5,7 +5,7 @@ use nom::error::Error;
 ///     │   username│   hostname │pathname  │   fragment      
 ///     │      │    │      │     │   │      │    │            
 ///    ┌┴─┐   ┌┴─┐ ┌┴─┐ ┌──┴───┐ ├┐┌─┴──┐ ┌─┴─┐ ┌┴─┐          
-///    http://user:pass@site.com:80/pa/th?q=val#hash          
+///    http://user:pass@site.com:80/pa/th?q=val&s=x#hash          
 ///    │                └────┬────┘└────┬─────┘    │          
 ///    │                    host       path        │          
 ///    └───────────────────┬───────────────────────┘          
@@ -26,7 +26,7 @@ type Query = HashMap<String, String>;
 
 #[derive(PartialEq, Debug)]
 enum Target {
-    Origin(Absolute<Path>, Query),
+    Origin(Absolute<Path>, Option<Query>),
     Absolute(URI),
     Authority(Host),
     Asterix,
@@ -50,9 +50,9 @@ struct Host {}
 use nom::branch::alt;
 use nom::bytes::complete::{is_not, tag, take_till, take_until, take_while, take_while1};
 use nom::character::complete::{anychar, char, one_of, satisfy};
-use nom::combinator::{all_consuming, eof, map, not, opt, peek, recognize};
-use nom::multi::{many1, separated_list0};
-use nom::sequence::{preceded, terminated, tuple};
+use nom::combinator::{all_consuming, eof, map, not, opt, peek, recognize, success};
+use nom::multi::{many1, separated_list0, separated_list1};
+use nom::sequence::{preceded, separated_pair, terminated, tuple};
 
 pub trait Parsable {
     type Output;
@@ -60,7 +60,7 @@ pub trait Parsable {
 
     fn parse(i: &str) -> Result<Self::Output, ()> {
         match Self::nom_parse(i) {
-            Ok((surplus, result)) => Ok(result),
+            Ok((_, result)) => Ok(result),
             Err(_) => {
                 eprintln!("parse error");
                 Err(())
@@ -68,11 +68,17 @@ pub trait Parsable {
         }
     }
 }
+
+/// Contains all characters in ascii, separated into url charsets
+#[allow(dead_code)]
 mod ascii_charsets {
     pub const NUMERIC: &'static str = "0123456789";
     pub const ALPHA_SMALL: &'static str = "abcdefghijklmnopqrstuvwxyz";
     pub const ALPHA_CAPITAL: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    pub const CONTROL: &'static str = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1d\x1E\x1F";
+    pub const CONTROL: &'static str = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\
+                                      \x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\
+                                      \x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\
+                                      \x1d\x1E\x1F";
 
     pub const URL_UNRESERVED: &'static str = "-._~";
     pub const GEN_DELIMS: &'static str = ":/?#[]@";
@@ -85,7 +91,7 @@ fn is_url_terminative(c: char) -> bool {
     use ascii_charsets::{CONTROL, URL_ILLEGAL};
     c.is_ascii() && (URL_ILLEGAL.contains(c) || CONTROL.contains(c))
 }
-fn end(i: &str) -> IResult<&str, ()> {
+fn url_end(i: &str) -> IResult<&str, ()> {
     match eof::<&str, ()>(i) {
         Ok(_) => Ok((i, ())),
         Err(_) => match satisfy::<_, _, ()>(is_url_terminative)(i) {
@@ -106,6 +112,16 @@ fn end(i: &str) -> IResult<&str, ()> {
 //     fn nom_parse<'a>(i: &'a str) -> IResult<&str, Query> {}
 // }
 
+impl Parsable for Target {
+    type Output = Self;
+    fn nom_parse(i: &str) -> IResult<&str, Self> {
+        alt((
+            map(tuple((char('*'), url_end)), |_| Self::Asterix),
+            map(tuple((char('*'), url_end)), |_| Self::Asterix),
+        ))(i)
+    }
+}
+
 impl Parsable for Relative<Path> {
     type Output = Relative<Path>;
     fn nom_parse<'a>(i: &'a str) -> IResult<&str, Relative<Path>> {
@@ -116,7 +132,7 @@ impl Parsable for Relative<Path> {
                 }
                 let (sur, res) = separated_list0(
                     char('/'),
-                    take_while1(|c| c != '/' && c != '?' && !is_url_terminative(c)),
+                    take_while1(|c| c != '/' && c != '?' && c != ';' && !is_url_terminative(c)),
                 )(i)?;
                 let (stripped, _) = take_maybe_leading_slash(sur)?;
                 Ok((stripped, res))
@@ -136,15 +152,48 @@ impl Parsable for Absolute<Path> {
     }
 }
 
-impl Parsable for Target {
-    type Output = Self;
-    fn nom_parse(i: &str) -> IResult<&str, Self> {
-        alt((
-            map(tuple((char('*'), end)), |_| Self::Asterix),
-            map(tuple((char('*'), end)), |_| Self::Asterix),
-        ))(i)
+impl Parsable for Option<Query> {
+    type Output = Option<Query>;
+    fn nom_parse(i: &str) -> IResult<&str, Option<Query>> {
+        match char::<&str, ()>('?')(i) {
+            Ok(_) => match Query::nom_parse(i) {
+                Ok((sur, res)) => Ok((sur, Some(res))),
+                Err(_) => Err(nom::Err::Error(Error {
+                    input: i,
+                    code: nom::error::ErrorKind::Permutation,
+                })),
+            },
+            Err(_) => success(None)(i),
+        }
     }
 }
+
+impl Parsable for Query {
+    type Output = Query;
+    fn nom_parse(i: &str) -> IResult<&str, Query> {
+        map(
+            preceded(
+                char('?'),
+                separated_list1(
+                    char('&'),
+                    separated_pair(
+                        take_while1(|c| c != '=' && c != '&' && !is_url_terminative(c)),
+                        char('='),
+                        take_while1(|c| c != '=' && c != '&' && !is_url_terminative(c)),
+                    ),
+                ),
+            ),
+            |l: Vec<(&str, &str)>| {
+                let mut map: HashMap<String, String> = HashMap::new();
+                for (key, val) in l {
+                    map.insert(key.to_string(), val.to_string());
+                }
+                map
+            },
+        )(i)
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -157,6 +206,7 @@ mod tests {
 
     #[test]
     fn test_relative_path() {
+        assert_eq!(Relative::<Path>::parse("").unwrap(), Relative(vec![]));
         assert_eq!(
             Relative::<Path>::parse("hello").unwrap(),
             Relative(vec!["hello".to_owned()])
@@ -196,6 +246,51 @@ mod tests {
         assert_eq!(
             Absolute::<Path>::parse("/hello/there/").unwrap(),
             Absolute(vec!["hello".to_owned(), "there".to_owned()])
+        );
+    }
+    #[test]
+    fn test_query() {
+        let mut t_hei_der: HashMap<String, String> = HashMap::new();
+        t_hei_der.insert("hei".to_string(), "der".to_string());
+        let mut t_double: HashMap<String, String> = HashMap::new();
+        t_double.insert("hei".to_string(), "der".to_string());
+        t_double.insert("ddd".to_string(), "padeg".to_string());
+
+        assert_eq!(Query::parse("?hei=der").unwrap(), t_hei_der);
+        assert_eq!(Query::parse("?hei=der&ddd=padeg").unwrap(), t_double);
+        assert_eq!(Query::nom_parse("?hei=der"), Ok(("", t_hei_der.clone())));
+        assert_eq!(
+            Query::nom_parse("?hei=der&ddd=padeg"),
+            Ok(("", t_double.clone()))
+        );
+        assert_eq!(Query::nom_parse("?hei=der "), Ok((" ", t_hei_der.clone())));
+        assert_eq!(
+            Query::nom_parse("?hei=der&ddd=padeg\n"),
+            Ok(("\n", t_double.clone()))
+        );
+        assert!(Query::parse("?hei=").is_err());
+        assert!(Query::parse("?=").is_err());
+        assert!(Query::parse("?").is_err());
+        assert!(Query::parse("?=3").is_err());
+    }
+    #[test]
+    fn test_optional_query() {
+        assert_eq!(Option::<Query>::parse("").unwrap(), None);
+        assert!(Option::<Query>::parse("?=3").is_err());
+        assert!(Option::<Query>::parse("?&").is_err());
+
+        let mut t_single: HashMap<String, String> = HashMap::new();
+        t_single.insert("mes".to_string(), "abc".to_string());
+        let mut t_double: HashMap<String, String> = HashMap::new();
+        t_double.insert("__".to_string(), "~".to_string());
+        t_double.insert("r".to_string(), "ff".to_string());
+        assert_eq!(
+            Option::<Query>::parse("?mes=abc").unwrap(),
+            Some(t_single.clone())
+        );
+        assert_eq!(
+            Option::<Query>::parse("?__=~&r=ff").unwrap(),
+            Some(t_double.clone())
         );
     }
     #[test]
